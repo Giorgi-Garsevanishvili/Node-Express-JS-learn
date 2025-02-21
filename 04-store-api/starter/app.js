7934cf8aeae93f0ac8955c92d1b6818e2714cb1f
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2025-02-21 20:37:54 +0200

    added controlers, Schema, routes and add data automaticaly (from populate json)...

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require('express-async-errors')
 
 const express = require("express");
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 //async errors
 
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleWare = require("./middleware/not-found");
 const errorMiddleWare = require("./middleware/error-handler");
@@ -19,6 +21,8 @@ app.get("/", (req, res) => {
   res.send(`<h1>Store API</h1><a href="/api/v1/products">Products Route</a>`);
 });
 
+
+app.use('/api/v1/products', productsRouter)
 
 
 // products route
