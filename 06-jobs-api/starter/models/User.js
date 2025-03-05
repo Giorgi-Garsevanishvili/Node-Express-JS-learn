66112f3eb71ec7e75bd21b62552f3fa543846cea
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2025-03-05 17:59:26 +0200

    shcema.pre is setted for password hash during the registration as well as token generation by JTW package and bcryptjs for password hash, registration is functional and log in basic function works

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
   name:{
     type:String,
-    required: [true,'Please prove name'],
+    required: [true,'Please provide name'],
     minlength: 3,
     maxlength: 50,
   },
@@ -17,8 +19,16 @@ const userSchema = mongoose.Schema({
     type:String,
     require: [true, 'please provide password'],
     minlength: 6,
-    maxlength: 20,
   }
 })
+
+userSchema.pre('save', async function(){
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+userSchema.methods.createJWT = function (){
+ return jwt.sign({userID:this._id, name:this.name},process.env.JWT_SECRET, {expiresIn:process.env.JWT_LIFETIME} )
+}
 
 module.exports = mongoose.model('User', userSchema)
